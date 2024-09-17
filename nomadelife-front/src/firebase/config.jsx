8e735c2d865d483681c1e19e76f5fc477edef06d
DanielFbononi <daniel.bononi@fatec.sp.gov.br>
2024-09-17 12:32:03 -0300

    aula0917-useAuthentication based code

---
+++ nomadelife-front/src/firebase/config.jsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import  { getFireStore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTTdxG3pZuegv2j5LvC3qfvsteH928yOY",
+  authDomain: "nomadelife-dj.firebaseapp.com",
+  projectId: "nomadelife-dj",
+  storageBucket: "nomadelife-dj.appspot.com",
+  messagingSenderId: "1097712633023",
+  appId: "1:1097712633023:web:f8102155f7d2788134f20a",
+  measurementId: "G-75MWPLS5JC"
+};
+
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const db = getFireStore(app)
+
+export {db,analytics}
