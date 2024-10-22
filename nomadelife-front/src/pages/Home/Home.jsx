6bb93beb2090d14e3480896f0b8a23c54eec5c97
DanielFBononi <dnielbf21@gmail.com>
2024-10-22 13:18:31 -0300

    atualizaçao

--- nomadelife-front/src/pages/Home/Home.jsx
+++ nomadelife-front/src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import logo from '/LogoQuadrado.png'
 
@@ -11,17 +10,3 @@ const Home = () => {
 }
 
 export default Home
-=======
-import React from 'react'
-import logo from '/logo-quadrada.png'
-
-const Home = () => {
-  return (
-    <div>
-        <img src={logo} alt="Logo da desenvolvedora" />
-    </div>
-  )
-}
-
-export default Home
->>>>>>> a48cf12a17b319bfc71cb577f5c05faf9bc4f218
