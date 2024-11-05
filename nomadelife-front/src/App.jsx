ead75de50a8ee30f0c75d678d2ca5ac791ecd19a
DanielFbononi <daniel.bononi@fatec.sp.gov.br>
2024-11-05 12:40:15 -0300

    aula1105- rotas no app

--- nomadelife-front/src/App.jsx
+++ nomadelife-front/src/App.jsx
@@ -1,28 +1,55 @@
 import './App.css';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useAuthentication } from './hooks/useAuthentication';
+import About from './pages/About/About';
+import Dashboard from './pages/Dashboard/Dashboard';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import { useEffect } from 'react';
+import loading from '../public/loading.svg'
 
-function App() {
-const [count, setCount] = useState(0)
+const loadingUser = user === undefined
 
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <div className='container'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                </Routes>
-            </div>
-            <Footer />
-        </BrowserRouter>
-    );
+useEffect(() => {
+    onAuthStateChanged(auth,user => {
+        setUser(user)
+    })
+}, [auth])
+
+if (loadingUser) {
+    return <div className='container load'><img src={loading} width="120px" height="120px" alt="Loading Blog"></img></div>
 }
 
-export default App;
+function App() {
+    const { user, setUser } = useState(undefined)
+    const { auth } = useAuthentication()
+
+
+    return (
+        <>
+            <BrowserRouter>
+                <NavBar />
+                <div className='container'>
+                    <Routes>
+                        <Route path='/' element={<Home />}></Route>
+                        <Route path='/about' element={<About />}></Route>
+                        <Route path='/login' element={<Login />}></Route>
+                        <Route path='/register' element={<Register />}></Route>
+                        <Route path='/post/create' element={<CreatePost />}></Route>
+                        <Route path='/dashboard' element={<Dashboard />}></Route>
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App
---
+++ nomadelife-front/src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../../../public/LogoQuadado.png'
+
+export const About = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo desenvolvedora" ></img>
+    </div>
+  )
+}
